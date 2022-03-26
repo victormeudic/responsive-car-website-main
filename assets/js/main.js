6284c9fe58d3198be78f585fcc2f6fd269f0114e
Victor Meudic <victormeudic@gamil.com>
2022-03-26 17:07:36 +0100

    Scroll Up

--- assets/js/main.js
+++ assets/js/main.js
@@ -74,7 +74,13 @@ function activeFeatured() {
 }
 linkFeatured.forEach((l) => l.addEventListener("click", activeFeatured));
 /*=============== SHOW SCROLL UP ===============*/
-
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 350) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
